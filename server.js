127e352ef4613a535ed1074c7fa82047cdb8969a
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-12 15:50:47 -0500

    reorganized routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //Dependencies
 const express = require ('express');
+const path = require('path');
 
 //Routes
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/index.js');
 
 //Ports
 const PORT = process.env.PORT || 3001;
@@ -14,9 +14,23 @@ const app = express();
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', api);
 app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route
+app.all('*', (req,res) => {
+  res.status(404).send('<h1>404! Page Not Found <h1>');
+});
 
 //Listener
 app.listen(PORT, () =>
